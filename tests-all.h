5bbe6928c0577a477bdc5ddad3301fd3ed8739f3
GreyKnight <tinyplasticgreyknight@yahoo.com>
2014-05-29 22:34:05 +0100

    test for rule

--- tests-all.h
+++ tests-all.h
@@ -2,10 +2,12 @@ extern void register_tests_llist(void);
 extern void register_tests_state(void);
 extern void register_tests_colour(void);
 extern void register_tests_machine(void);
+extern void register_tests_rule(void);
 
 void register_tests(void) {
 	register_tests_llist();
 	register_tests_state();
 	register_tests_colour();
 	register_tests_machine();
+	register_tests_rule();
 }
