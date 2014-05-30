9e2c7bf8accbbc85c7dbb6f5b7fac6db2fc0ddf1
GreyKnight <tinyplasticgreyknight@yahoo.com>
2014-05-30 02:17:19 +0100

    reformat demo-unary

--- demo-unary.cpp
+++ demo-unary.cpp
@@ -1,27 +1,39 @@
-#include "demo.h"
+#include "llist.h"
+#include "rule.h"
+#include "ruleset.h"
+#include "state.h"
+#include "colour.h"
+#include "direction.h"
+#include "apply.h"
+
 #include <iostream>
 
-typedef Turing::machine<S(5), INFTAPE, Turing::EMPTY, INFTAPE> Machine;
+using namespace Turing;
 
-typedef RULES(
-	      RULE(S(5), Turing::EMPTY, S(4), C('!'), Turing::go_left),
-	      RULES(
-		    RULE(S(4), Turing::EMPTY, S(3), C('!'), Turing::go_left),
-		    RULES(
-			  RULE(S(2), Turing::EMPTY, S(1), C('!'), Turing::go_left),
-			  RULES(
-				RULE(S(3), Turing::EMPTY, S(2), C('!'), Turing::go_left),
-				RULES(
-				      RULE(S(1), Turing::EMPTY, Turing::HALT, C('!'), Turing::stay_put),
-				      RULES0
-				      )
-				)
-			  )
-		    )
-	      ) Rules;
+typedef state<5> InitialState;
+
+typedef ruleset<
+		rule<state<4>, EMPTY, state<3>, colour<'1'>, go_left>,
+		ruleset<
+		rule<state<3>, EMPTY, state<2>, colour<'1'>, go_left>,
+		ruleset<
+		rule<state<2>, EMPTY, state<1>, colour<'1'>, go_left>,
+		ruleset<
+		rule<state<5>, EMPTY, state<4>, colour<'1'>, go_left>,
+		ruleset<
+		rule<state<1>, EMPTY, HALT, colour<'1'>, stay_put>,
+		List::nil> > > > >
+	Rules;
+
+typedef Turing::machine<
+		InitialState, // machine state
+		List::infinite<EMPTY>,  // tape going to the left
+		EMPTY, // head cell
+		List::infinite<EMPTY> // tape going to the right
+	> Machine;
 
 int main(void) {
-	std::cout << Machine::to_str() << std::endl;
-	std::cout << Turing::fully_apply_rule<Machine, Rules>::to_str() << std::endl;
+	std::cout << "initial: " << Machine::to_str() << std::endl;
+	std::cout << "final:   " << Turing::fully_apply_rule<Machine, Rules>::to_str() << std::endl;
 	return 0;
 }
